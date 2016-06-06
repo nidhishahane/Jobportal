0e9aaf40222f0a5d3130385441e13160c24c9991
nidhi <nidhi.affimintus@gmail.com>
2016-06-06 16:56:03 +0530

    Added user profile part

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   
-  
-
   devise_for :users,controllers:{registrations: "registrations"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -23,7 +21,6 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'companies#index'
   
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
